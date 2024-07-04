096d15aa4cddea1ae2328679a61c8dcdce6c001e
Anthony Volk <anthony@policyengine.org>
2024-07-03 20:00:33 -0400

    chore: Changelog

--- policyengine_it/variables/household/person/employment_category.py
+++ policyengine_it/variables/household/person/employment_category.py
@@ -1,10 +1,12 @@
 from policyengine_it.model_api import *
 
+
 class EmploymentCategory(Enum):
     # This is not defined well in the reference material
     EMPLOYEE = "Standard employee"
     EXECUTIVE = "Executive"
     UNEMPLOYED = "Unemployed"
+
 
 class employment_category(Variable):
     value_type = Enum
