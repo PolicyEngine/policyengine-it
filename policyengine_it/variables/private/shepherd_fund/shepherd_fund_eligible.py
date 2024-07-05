74d2a6bed399e4abbe4f3315765edc9d9de376d6
Anthony Volk <anthony@policyengine.org>
2024-07-05 00:57:57 -0400

    feat: Shepherd Fund

---
+++ policyengine_it/variables/private/shepherd_fund/shepherd_fund_eligible.py
@@ -0,0 +1,23 @@
+from policyengine_it.model_api import *
+
+
+class shepherd_fund_eligible(Variable):
+    value_type = bool
+    entity = Person
+    label = "Eligible for Shepherd Fund"
+    definition_period = YEAR
+
+    def formula(person, period, parameters):
+        sf_eligible_groups = parameters(
+            period
+        ).private.shepherd_fund.eligibility
+
+        employment_category = person(
+            "employment_category", period
+        ).decode_to_str()[0]
+
+        is_eligible = np.where(
+            employment_category in sf_eligible_groups, True, False
+        )
+
+        return is_eligible
