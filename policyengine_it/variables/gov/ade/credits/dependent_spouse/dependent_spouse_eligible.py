08cffd051b6e6e6e65e1b0076b617f2776e9cbb3
Anthony Volk <anthony@policyengine.org>
2024-07-04 21:31:30 -0400

    fix: Remove dead code

--- policyengine_it/variables/gov/ade/credits/dependent_spouse/dependent_spouse_eligible.py
+++ policyengine_it/variables/gov/ade/credits/dependent_spouse/dependent_spouse_eligible.py
@@ -12,15 +12,12 @@ class dependent_spouse_eligible(Variable):
 
         # Determine if household is married
         household_is_married: bool = household("household_married", period)
-        print(household_is_married)
 
         # Pull overall household income pre-credits
         household_income: float = household("household_market_income", period)
-        print(household_income)
 
         # Find spouse's income specifically
         spouse_income: float = household("spouse_market_income", period)
-        print(spouse_income)
 
         # Find values for each eligibility criterium
         elig_income_dist: bool = household_income > spouse_income
@@ -28,18 +25,11 @@ class dependent_spouse_eligible(Variable):
         elig_household_income: bool = household_income <= p.household_income
         elig_marital_status: bool = household_is_married
 
-        print(elig_income_dist)
-        print(elig_spouse_income)
-        print(elig_household_income)
-        print(elig_marital_status)
-
         eligible = (
             elig_income_dist
             & elig_spouse_income
             & elig_household_income
             & elig_marital_status
         )
-
-        print(eligible)
 
         return eligible
