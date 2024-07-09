fc392f19c4a33471309bdeb54106c98c84132565
Anthony Volk <anthony@policyengine.org>
2024-07-09 01:22:53 -0400

    feat: Universal credit low-income bonus

--- policyengine_it/variables/gov/benefits.py
+++ policyengine_it/variables/gov/benefits.py
@@ -7,4 +7,8 @@ class benefits(Variable):
     label = "benefits"
     unit = EUR
     definition_period = YEAR
-    adds = ["inclusion_checks", "universal_credit"]
+    adds = [
+        "inclusion_checks",
+        "universal_credit",
+        "universal_credit_low_income_bonus",
+    ]
